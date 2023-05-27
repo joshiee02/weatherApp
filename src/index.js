f6b0ecb0dcfcd40f8286647a69d200724de985f1
joshiee02 <jkhirakosocmed@gmail.com>
2023-05-27 15:45:35 +0900

    add swiper lib for weekly forecast carousel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+     // adjust as needed for the gap between slides
+    navigation: {
+        nextEl: '#arrow',
+    },
+    allowTouchMove: true,
+    slidesPerGroup: 3,
+  });
+})
