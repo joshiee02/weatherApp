5fe92064699c1fd9ba33a9983d4c514b94239538
joshiee02 <jkhirakosocmed@gmail.com>
2023-06-01 20:43:00 +0900

    add api call for temperature

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
 import './style.css';
 
-document.addEventListener('DOMContentLoaded', function() {
-  var swiper = new Swiper('.swiper-container', {
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper-container', {
     slidesPerView: 4,
     spaceBetween: 20,
-     // adjust as needed for the gap between slides
+    // adjust as needed for the gap between slides
     navigation: {
-        nextEl: '#arrow',
+      nextEl: '#arrow',
     },
     allowTouchMove: true,
     slidesPerGroup: 3,
   });
-})
+});
+
+// const getWeather = async function () {
+//   try {
+
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+(function runsAtStart() {
+  async function fetchData(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      return null;
+    }
+  }
+
+  async function getTemperature() {
+    try {
+      const data = await fetchData('http://api.weatherapi.com/v1/current.json?key=5d8ec60449724cc5ad342032232605&q=Davao');
+      console.log(data);
+
+      const temperature = document.querySelector('#temperature');
+      temperature.textContent = `${data.current.temp_c}°`;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  getTemperature();
+}());
